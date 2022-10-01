cfb8f967a6c91de0c5be71926f1bd0ff625cadec
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-10-01 10:19:15 +0300

    Intermediate independent work

--- Makefile
+++ Makefile
@@ -2,11 +2,5 @@
 install:
 	composer install
 
-index:
-	php -S localhost:8080  src/$(file)/public/index.php
-
-lesson:
-	php -S localhost:8080  src/$(file)/public/lesson.php
-
 project:
 	php -S localhost:8080 $(file)/public/index.php
--- project-third/public/index.php
+++ project-third/public/index.php
@@ -6,19 +6,63 @@ require dirname(__DIR__) . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+// Старт PHP сессии
+session_start();
+
 $container = new Container();
 $container->set('renderer', function () {
-    // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-// $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
+});
+
+
+//------------FLASH
+$app->get('/foo', function ($req, $res) {
+    $this->get('flash')->addMessage('success', 'This is a message');
+    return $res->withRedirect('/bar');
+});
+$app->get('/bar', function ($req, $res) {
+    // Извлечение flash сообщений установленных на предыдущем запросе
+    $messages = $this->get('flash')->getMessages();
+    print_r($messages); // => ['success' => ['This is a message']]
+
+    $params = ['flash' => $messages];
+    return $this->get('renderer')->render($res, 'users/bar.phtml', $params);
+});
+//------------
+
+$app->get('/user/{id}', function ($request, $response, $args) {
+    $fileUsers = json_decode(file_get_contents('project-third/data.json'), true);
+    $nameId = '404';
+    foreach ($fileUsers as $user) {
+        if ($user['id'] === $args['id']) {
+            $nameId = $user['name'];
+            continue;
+        }
+    }
+    if ($nameId === '404') {
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Page not found');
+    }
+    $params = [
+        'id' => $args['id'],
+        'nickname' => $nameId
+    ];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->get('/users/new', function ($request, $response) {
@@ -34,6 +78,7 @@ $app->get('/users/new', function ($request, $response) {
 $app->get('/users', function ($request, $response) use ($users) {
     $term = $request->getQueryParam('term');
     $fileUsers = json_decode(file_get_contents('project-third/data.json'), true);
+
     if (!empty($fileUsers)) {
         foreach ($fileUsers as ['name' => $name]) {
             $users[] = $name;
@@ -46,7 +91,7 @@ $app->get('/users', function ($request, $response) use ($users) {
     $params = [
         'users' => $users,
         'fileUsers' => $fileUsers,
-        'term' => $term
+        'term' => $term,
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
@@ -60,7 +105,7 @@ $app->post('/users', function ($request, $response) {
 
     if (count($errors) === 0) {
         $fileUser = [];
-        if (file_exists('project-third/data.json') && filesize('project-third/data.json')) {
+        if (file_exists('project-third/data.json')) {
             $fileUser = json_decode(file_get_contents('project-third/data.json'), true);
         }
         $fileUser[] = $user;
